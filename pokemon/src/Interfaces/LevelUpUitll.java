102deec23097f92ce56e73e29458384e3941b92d
heung115 <heung115@naver.com>
2023-04-10 09:55:36 +0900

    add Interfaces

---
+++ pokemon/src/Interfaces/LevelUpUitll.java
@@ -0,0 +1,13 @@
+package Interfaces;
+
+
+interface LevelUpUInterface {
+  public void Levelup();
+}
+public class LevelUpUitll implements LevelUpUInterface{
+
+  public void Levelup(){
+    System.out.print("level up");
+  }
+  
+}
