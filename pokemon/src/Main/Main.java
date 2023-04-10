34182ee28e1364a3273fa1a17a4d4e62a4c06135
heung115 <heung115@naver.com>
2023-04-10 09:51:30 +0900

    add Main.jave

--- pokemon/src/Main/Main.java
+++ pokemon/src/Main/Main.java
@@ -1,7 +1,22 @@
 package Main;
+import Player.*;
+
+import java.util.Scanner;
 
 public class Main {
+  static Scanner sc = new Scanner(System.in);
+  //static Player player;
+
+  public static Player createPlayerInstance(){
+    System.out.print("플레이어의 이름 : ");
+		String name = sc.next();
+    Player player = new Player(name);
+    return player;
+  }
   public static void main(String[] args) throws Exception {
-      System.out.println("Hello, World!");
+
+    Player player = createPlayerInstance();
+    System.out.println(player.getName());
+    System.out.println(player.getLevel());
   }
 }
