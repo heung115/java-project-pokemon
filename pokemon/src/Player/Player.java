6559396a31cb270f25b7c554222a79d1252b3292
heung115 <heung115@naver.com>
2023-04-10 09:51:49 +0900

    add Player.jave

---
+++ pokemon/src/Player/Player.java
@@ -0,0 +1,24 @@
+package Player;
+import Interfaces.LevelUpUitll;
+
+public class Player {
+  private String playerName;
+  private int level;
+  private int exp;
+  LevelUpUitll levelUpUitll = new LevelUpUitll();
+
+  public int getLevel(){
+    return level;
+  }
+  public String getName(){
+    return playerName;
+  }
+  public int getExp(){
+    return exp;
+  }
+
+  public Player(String playerName){
+    this.playerName = playerName; 
+    level = 1;
+  }
+}
