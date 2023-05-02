4520da22e865e875c5f24c5ef0e776c27884cbe3
heung115 <heung115@naver.com>
2023-05-02 17:21:17 +0900

    add player.changePokemon

--- pokemon/src/Player/Player.java
+++ pokemon/src/Player/Player.java
@@ -1,6 +1,7 @@
 package Player;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import Pokemon.Pokemon;
 
 public class Player {
@@ -11,6 +12,12 @@ public class Player {
   ArrayList<Pokemon> playerPokemon = new ArrayList<>();
   private Bag playerBag = new Bag();
   private Encyclopedia playerEncyclopedia = new Encyclopedia();
+
+  public Player(String playerName) {
+    this.playerName = playerName;
+    // level = 1;
+    // currentExp = 0;
+  }
 
   public String getName() {
     return playerName;
@@ -25,15 +32,24 @@ public class Player {
     playerBag.showBag();
   }
 
+  /*
+   * for pokemon
+   */
+  public void printPlayerPokemon() {
+    for (int i = 0; i < playerPokemon.size(); i++) {
+      Pokemon tempPokemon = playerPokemon.get(i);
+      tempPokemon.showAllStat();
+      // TODO 포켓몬 정보 출력 부분 추가
+    }
+  }
+
+  public void changePokemon(int num1, int num2) {
+    Collections.swap(playerPokemon, num1, num2);
+  }
+
   public void addPokemonToPlayerPokemonArrayList() {
     Pokemon tempPokemon = new Pokemon();
     playerPokemon.add(tempPokemon);
-  }
-
-  public Player(String playerName) {
-    this.playerName = playerName;
-    // level = 1;
-    // currentExp = 0;
   }
 
   /*
