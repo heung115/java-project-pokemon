4104e3ad8c599c9678c1a555d486ffbdca04d8d2
Jeemin <cjmin2004@naver.com>
2023-05-02 19:27:58 +0900

    fix Poketmon.java : using encyclopedia

--- pokemon/src/Pokemon/Pokemon.java
+++ pokemon/src/Pokemon/Pokemon.java
@@ -1,10 +1,9 @@
 package Pokemon;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException; //Input,Output 예외처리 
-import java.util.StringTokenizer;
+import Player.Encyclopedia.encyclopedia;
 import java.util.Random;
+
 
 public class Pokemon {
 	private String pokemonName;
@@ -18,59 +17,40 @@ public class Pokemon {
 	private int evolutionLevel;
 	private int evolutionNo;
 
-	// read 1 each line
-	public void getPokeInfo() throws IOException {
-		
-		BufferedReader br = new BufferedReader(new FileReader("pokemon\\src\\Pokemon\\pokemonList.txt"));
-		br.readLine();	//skip 1 line (index name)
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		//reading
-		pokemonNumber =Integer.parseInt(st.nextToken());
-		pokemonName = st.nextToken();
-		type = Integer.parseInt(st.nextToken());
-		maxHp = Integer.parseInt(st.nextToken());
-		damage = Integer.parseInt(st.nextToken());
-		evolution = Boolean.valueOf(st.nextToken());
-		evolutionLevel = Integer.parseInt(st.nextToken());
-		evolutionNo = Integer.parseInt(st.nextToken());
-		
-		//print infomation of pokemon
-		System.out.println(pokemonNumber + " " + pokemonName + " " + type + " " + maxHp + " " + damage + " " + evolution + " " + evolutionLevel + " " + evolutionNo);
+	//creat pokemon : decided pokemonNumber 
+	public void makePokemon(int num){
+		pokemonName = encyclopedia.get(num).get(0);
+		type = encyclopedia.get(num).get(1);
+		maxHp = currentHp = encyclopedia.get(num).get(2);
+		damage = encyclopedia.get(num).get(3);
+	};
 
-		br.close();
-	}
+	//creat poketmon : decided type
+	public void makePokemon(String type){
+		String str = "";
+
+		for(int i=0; i < encyclopedia.size() ; i++){
+			str = encyclopedia.get(i).get(4);
+			if(str.equals(type)){
+				break;
+			}	
+		}
+
+		pokemonName = encyclopedia.get(--i).get(1);
+		maxHp = currentHp = encyclopedia.get(i).get(2);
+		damage = encyclopedia.get(i).get(3);
+		type = encyclopedia.get(i).get(4);
+
+	};
 
 	//creat wild pokemon
-	public void makeWildPokemon() throws IOException {
+	public void makeWildPokemon(){
 		Random random = new Random();
-		int a = random.nextInt(3)+1;
-		System.out.println(a);
-		String pokeInfo="";
-
-		BufferedReader br = new BufferedReader(new FileReader("pokemon\\src\\Pokemon\\pokemonList.txt"));
-		StringTokenizer st;
-		br.readLine(); // skip line 1 : index name
-
-		for(int i = 0; i!=a; i++){
-			pokeInfo = br.readLine();
-		}
-		
-		st = new StringTokenizer(pokeInfo);
-		pokemonNumber =Integer.parseInt(st.nextToken());
-		pokemonName = st.nextToken();
-		type = Integer.parseInt(st.nextToken());
-		maxHp = currentHp = Integer.parseInt(st.nextToken());
-		damage = Integer.parseInt(st.nextToken());
-		belong = 1;
-
-		br.close();
-
-		/* To debug */
-		System.out.println(pokemonNumber + " " + pokemonName + " " + type + " " + maxHp + " " + damage + " ") ;
-
+		int a = random.nextInt(encyclopedia.size())+1;
+		makePokemon(a);
 	}
-
+	
+	
 	public void showAllStat() {
 		System.out.println(pokemonName);
 		System.out.println("max hp : " + maxHp);
@@ -98,18 +78,14 @@ public class Pokemon {
 		return this.currentHp;
 	}
 
-	private void startStatSetting() {
-
-	}
-
 	/*To debug*/
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 	
 		Pokemon poke[] = new Pokemon[2];
 		poke[0] = new Pokemon();
 		poke[1] = new Pokemon();
-		poke[0].getPokeInfo();
-		poke[1].makeWildPokemon();
+		//poke[0].getPokeInfo();
+		//poke[1].makeWildPokemon();
 	
 	  }
 }
