bb291b76163437a2cca36e94690d544fd3cfd311
heung115 <heung115@naver.com>
2023-05-17 15:47:48 +0900

    가방에서 아이템 추가 및 아이템 출력 추가

--- pokemon/src/Player/Bag.java
+++ pokemon/src/Player/Bag.java
@@ -1,26 +1,25 @@
 package Player;
 
 import java.util.ArrayList;
+import Player.Item.*;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Bag {
   // 2차원 동적 배열
-  ArrayList<ArrayList<Integer>> bag = new ArrayList<ArrayList<Integer>>();
+  ArrayList<ArrayList<Object>> bag = new ArrayList<>();
 
   public Bag() {
-    for (int i = 0; i < 10; i++) {
-      ArrayList<Integer> temp = new ArrayList<Integer>();
-      for (int j = 0; j < 2; j++) {
-        temp.add(0);
-      }
-      bag.add(temp);
-    }
   }
 
   public void showBag() {
+    if (bag.isEmpty()) {
+      System.out.println("가방이 비었습니다");
+    }
     for (int i = 0; i < bag.size(); i++) {
-      for (int j = 0; j < bag.get(i).size(); j++) {
-        System.out.print(bag.get(i).get(j) + " ");
-      }
+      System.out.print(i + ":");
+      Item item = (Item) bag.get(i).get(1);
+      System.out.println(item.getItemName() + "," + (int) bag.get(i).get(2) + "개");
       System.out.println();
     }
   }
@@ -29,7 +28,31 @@ public class Bag {
 
   }
 
-  public void addItem(int itemNum, int itemCount) {
+  /*
+   * 가방에서의 아이템 인덱스
+   * 중요!!! 아이템 매개변수를 넘길 때 업캐스팅해서 넘겨야함!!!!!!!
+   * 아이템 인덱스
+   * 아이템 객체
+   * 아이템 수
+   */
+  public void addItem(Item item, int itemCount) {
+    int bagSize = bag.size();
+    for (int i = 0; i < bagSize; i++) {
+      if (((Item) bag.get(i).get(1)).getItemName().equals(item.getItemName())) {
+        bag.get(i).set(2, ((Integer) bag.get(i).get(2)).intValue() + itemCount);
+        sortBag();
+        return;
+      }
+    }
+    ArrayList<Object> row = new ArrayList<>();
+    row.add(item.getItemIndex());
+    row.add(item);
+    row.add(itemCount);
+    bag.add(row);
+    sortBag();
+  }
 
+  private void sortBag() {
+    Collections.sort(bag, Comparator.comparingInt(row -> (int) row.get(0)));
   }
 }
