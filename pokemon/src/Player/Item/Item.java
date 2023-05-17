7e2312eac5892bd50423d424e4ea62c2616883e7
heung115 <heung115@naver.com>
2023-05-17 15:47:04 +0900

    아이템 클래스와 몬스터볼 자식 클래스 추가

---
+++ pokemon/src/Player/Item/Item.java
@@ -0,0 +1,22 @@
+package Player.Item;
+
+public class Item {
+    int itemIndex = 0;
+    String itemName;
+
+    /*
+     * itemIndex 1 : 몬스터볼
+     * itemIndex 2 : 상처약
+     */
+    public void use() {
+
+    }
+
+    public int getItemIndex() {
+        return itemIndex;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+}
---
+++ pokemon/src/Player/Item/MonsterBall.java
@@ -0,0 +1,49 @@
+package Player.Item;
+
+import Player.Player;
+import Pokemon.Pokemon;
+
+public class MonsterBall extends Item {
+    int monsterBallKind = 0;
+    static double[] captureRate = new double[] { 1, 1.5, 2, 255 };
+
+    public MonsterBall(String monsterBallName) {
+        itemIndex = 1;
+        switch (monsterBallName) {
+            case "몬스터볼":
+                this.monsterBallKind = 0;
+                this.itemName = "몬스터볼";
+                break;
+            case "슈퍼볼":
+                this.monsterBallKind = 1;
+                this.itemName = "슈퍼볼";
+                break;
+            case "하이퍼볼":
+                this.monsterBallKind = 2;
+                this.itemName = "하이퍼볼";
+                break;
+            case "마스터볼":
+                this.monsterBallKind = 3;
+                this.itemName = "마스터볼";
+                break;
+            default:
+                throw new IllegalArgumentException("몬스터볼의 이름을 다시 설정해주세요");
+        }
+    }
+
+    public void use(Player player, Pokemon targetPokemon) {
+        System.out.println(captureRateFormula(targetPokemon));
+        if (Math.random() < captureRateFormula(targetPokemon)) {
+            // capture!
+            player.addPokemonToPlayerPokemonArrayList(targetPokemon);
+        } else {
+            System.out.println("실패!!!");
+        }
+    }
+
+    private double captureRateFormula(Pokemon targetPokemon) {
+        return (3 * (double) targetPokemon.getMaxHp()
+                - 2 * (double) targetPokemon.getCurrentHp() * captureRate[monsterBallKind])
+                / (3 * (double) targetPokemon.getMaxHp());
+    }
+}
