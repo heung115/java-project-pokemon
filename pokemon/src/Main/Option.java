b1de59dcee89dfd9fa10bae2d21f78f05ae8a1e5
heung115 <heung115@naver.com>
2023-05-19 10:38:08 +0900

    옵션클래스 추가 및 구현 완료

---
+++ pokemon/src/Main/Option.java
@@ -0,0 +1,35 @@
+package Main;
+
+import Player.Player;
+import java.util.Scanner;
+
+public class Option {
+    Scanner scanner = new Scanner(System.in);
+    private int choice = 0;
+
+    public Option() {
+
+    }
+
+    public void mainOptionLoop(Player player) {
+        while (choice != 4) {
+            System.out.println("1.가방보기 \n2.포켓몬 보기 \n3.도감보기\n4.돌아가기");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("bag");
+                    player.showBag();
+                    break;
+                case 2:
+                    player.showPlayerPokemon();
+                    break;
+                case 3:
+                    player.ShowEncyclopedia();
+                    break;
+                case 4:
+                    break;
+            }
+        }
+    }
+}
