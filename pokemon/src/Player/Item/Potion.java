9fa82271fdc296bcc269d50c27d5493bd53f2be9
heung115 <heung115@naver.com>
2023-05-31 13:00:38 +0900

    인덱스 오류 및 채력 회복 내용에 관한 출력 부분 추가

--- pokemon/src/Player/Item/Potion.java
+++ pokemon/src/Player/Item/Potion.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 import Player.Player;
 
 public class Potion extends Item {
-    transient Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     public void use(Player player) {
         System.out.println("사용할 포켓몬을 골라주세요");
         player.showPlayerPokemon();
-        int num = scanner.nextInt();
+        int num = scanner.nextInt() - 1;
+        System.out.println(num);
         int healAmount = 100;
-        player.setPlayerPokemonHp(num, healAmount);
+
+        player.setPlayerPokemonHp(num, -healAmount);
+        System.out.println(player.getPlayerPokemonName(num) + "의 채력이 " + healAmount + "만큼 회복되었습니다");
     }
 
     public Potion() {
